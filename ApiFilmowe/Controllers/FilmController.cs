4d177c28ac9acb0c09ec65b7ab54cc8850e93c04
Kacper Niewczas <niewczas987@outlook.com>
2020-04-19 17:03:00 +0200

    19.04.2020 - ApiFilmowe

---
+++ ApiFilmowe/Controllers/FilmController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ApiFilmowe.Modele;
+
+namespace ApiFilmowe.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FilmController : ControllerBase
+    {
+        private readonly BazaFilmowaContext _context;
+
+        public FilmController(BazaFilmowaContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Film
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Film>>> GetFilm()
+        {
+            return await _context.Film.ToListAsync();
+        }
+
+        // GET: api/Film/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Film>> GetFilm(long id)
+        {
+            var film = await _context.Film.FindAsync(id);
+
+            if (film == null)
+            {
+                return NotFound();
+            }
+
+            return film;
+        }
+
+        // PUT: api/Film/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutFilm(long id, Film film)
+        {
+            if (id != film.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(film).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!FilmExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Film
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPost]
+        public async Task<ActionResult<Film>> PostFilm(Film film)
+        {
+            _context.Film.Add(film);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetFilm", new { id = film.Id }, film);
+        }
+
+        // DELETE: api/Film/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Film>> DeleteFilm(long id)
+        {
+            var film = await _context.Film.FindAsync(id);
+            if (film == null)
+            {
+                return NotFound();
+            }
+
+            _context.Film.Remove(film);
+            await _context.SaveChangesAsync();
+
+            return film;
+        }
+
+        private bool FilmExists(long id)
+        {
+            return _context.Film.Any(e => e.Id == id);
+        }
+    }
+}
